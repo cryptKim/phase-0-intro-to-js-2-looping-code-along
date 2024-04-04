dc7457966b6805a852fab49bb97030de4f0571c5
Kevin <yaiguru90@gmail.com>
2024-04-04 08:58:57 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function countDown(start) {
+    while (start >= 0) {
+      console.log(start);
+      start--;
+    }
+  }
+  countDown(10);
+  
+  function writeCards(names, eventName) {
+      let thankYouMessages = [];
+      
+      for (let i = 0; i < names.length; i++) { 
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        thankYouMessages.push(message); 
+      }
+      
+      return thankYouMessages;
+    }
